0a8e8cbc863893f2db29dbbe2dc33eb9e1bfa294
spotifyaegeus-boop <spotify.aegeus@gmail.com>
2026-08-01 02:16:12 +0800

    Update v.3.5 page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,23 @@
-import TravelApp from '../components/TravelApp';import {getTravelData} from '../lib/travel';export default async function Page(){const days=await getTravelData();return <TravelApp days={days}/>}
+import TravelApp from '../components/TravelApp';
+
+import {
+  getTravelData,
+  getTravelDocuments,
+  getPackingItems,
+} from '../lib/travel';
+
+export default async function Page() {
+  const [days, documents, packingItems] = await Promise.all([
+    getTravelData(),
+    getTravelDocuments(),
+    getPackingItems(),
+  ]);
+
+  return (
+    <TravelApp
+      days={days}
+      documents={documents}
+      packingItems={packingItems}
+    />
+  );
+}
